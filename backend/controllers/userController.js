cdc13ec53bdee2627d1453a9eeee3ae2b35c6f5c
codebysana <sana.younis05@gmail.com>
2025-08-05 07:56:45 +0500

    Work Complete

---
+++ backend/controllers/userController.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const { upload } = require("../multer");
+const ErrorHandler = require("../utils/errorHandler");
+const User = require("../models/userModel");
+const router = express.Router();
+
+router.post("/create-user", upload.single("file"), async (req, res) => {
+  const { name, email, password } = req.body;
+  const userEmail = await User.findOne({ email });
+  if (userEmail) {
+    return next(new ErrorHandler("User already exists", 400));
+  }
+  const filename = req.file.filename;
+  const fileUrl = path.join(filename);
+  const user = {
+    name: name,
+    email: email,
+    password: password,
+    avatar: fileUrl,
+  };
+  console.log(user);
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const app = require("./app");
+const connectToDB = require("./db/database");
+
+// handling uncaught exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server for handling uncaught exception`);
+});
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "backend/config/.env",
+  });
+}
+
+// connect db
+connectToDB();
+
+// create server
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
+
+// unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Shutting down the server for ${err.message}`);
+  console.log(`Shutting down th server for unhandled promise rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// tR40PvsVjst42p3J
