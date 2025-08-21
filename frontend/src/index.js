e384e4a1a15cdac60be7df68944836f66c7bcd1d
codebysana <sana.younis05@gmail.com>
2025-08-21 05:12:13 +0500

    Updated index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,3 @@
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import { Provider } from "react-redux";
-// import store from "./redux/store";
-
-// ReactDOM.createRoot(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById("root")
-// );
-
 // new Version
 
 import ReactDOM from "react-dom/client";
