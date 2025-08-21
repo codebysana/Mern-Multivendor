ef1882ff0eb42b3fdd7356d47bb4175fc78b6d58
codebysana <sana.younis05@gmail.com>
2025-08-21 17:06:44 +0500

    Updated sellerReducer

--- frontend/src/redux/reducers/sellerReducer.js
+++ frontend/src/redux/reducers/sellerReducer.js
@@ -1,24 +1,29 @@
 import { createReducer } from "@reduxjs/toolkit";
 
 const initialState = {
+  data: [],
+  seller: null,
+  error: null,
   isSeller: false,
+  isLoading: true,
 };
 
-export const sellerReducer = createReducer(initialState, {
-  LoadSellerRequest: (state) => {
-    state.isLoading = true;
-  },
-  LoadSellerSuccess: (state, action) => {
-    state.isSeller = true;
-    state.isLoading = false;
-    state.seller = action.payload;
-  },
-  LoadSellerFail: (state, action) => {
-    state.isLoading = false;
-    state.error = action.payload;
-    state.isSeller = false;
-  },
-  clearErrors: (state) => {
-    state.error = null;
-  },
+export const sellerReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase('LoadSellerRequest', (state) => {
+      state.isLoading = true;
+    })
+    .addCase('LoadSellerSuccess', (state, action) => {
+      state.isSeller = true;
+      state.isLoading = false;
+      state.seller = action?.payload;
+    })
+    .addCase('LoadSellerFail', (state, action) => {
+      state.isLoading = false;
+      state.error = action?.payload || "An error occurred";
+      state.isSeller = false;
+    })
+    .addCase('clearErrors', (state) => {
+      state.error = null;
+    });
 });
