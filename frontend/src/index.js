0038da5f7632a6e5afab9194dd9a40f3682e967a
codebysana <sana.younis05@gmail.com>
2025-08-21 05:11:16 +0500

    Updated index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,27 @@
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import { Provider } from "react-redux";
+// import store from "./redux/store";
+
+// ReactDOM.createRoot(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById("root")
+// );
+
+// new Version
+
+import ReactDOM from "react-dom/client";
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
