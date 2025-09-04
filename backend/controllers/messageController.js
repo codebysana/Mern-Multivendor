8b09fd288f2910ac4cf5455dddbfd5932700646d
codebysana <sana.younis05@gmail.com>
2025-09-05 00:28:14 +0500

    Added messages with conversation id

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -19,9 +19,11 @@ router.post(
       }
       messageData.conversationId = req.body.conversationId;
       messageData.sender = req.body.sender;
+      messageData.text = req.body.text;
 
       const message = new Message({
         conversationId: messageData.conversationId,
+        text: messageData.text,
         sender: messageData.sender,
         images: messageData.images ? messageData.images : undefined,
       });
@@ -29,6 +31,24 @@ router.post(
       res.status(201).json({
         success: true,
         message,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.response.message, 500));
+    }
+  })
+);
+
+// get all messages with conversation id
+router.get(
+  "/get-all-messages/:id",
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const messages = await Message.find({
+        conversationId: req.params.id,
+      });
+      res.status(201).json({
+        success: true,
+        messages,
       });
     } catch (error) {
       return next(new ErrorHandler(error.response.message, 500));
