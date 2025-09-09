cf5b38f7498567f1cfa10ae804ed403e40d759af
codebysana <sana.younis05@gmail.com>
2025-09-09 05:52:52 +0500

    Work Complete

---
+++ frontend/src/components/admin/AllEvents.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { AiOutlineDelete, AiOutlineEye } from "react-icons/ai";
+import { Button } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import axios from "axios";
+import { server } from "../../server";
+
+const AllEvents = () => {
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${server}/event/admin-all-events`, { withCredentials: true })
+      .then((res) => {
+        setEvents(res.data.events);
+      });
+  }, []);
+
+  const columns = [
+    { field: "id", headerName: "Product Id", minWidth: 150, flex: 0.7 },
+    { field: "name", headerName: "Name", minWidth: 180, flex: 1.4 },
+    { field: "price", headerName: "Price", minWidth: 100, flex: 0.6 },
+    {
+      field: "stock",
+      headerName: "Stock",
+      type: "number",
+      minWidth: 80,
+      flex: 0.5,
+    },
+    {
+      field: "sold",
+      headerName: "Sold out",
+      type: "number",
+      minWidth: 130,
+      flex: 0.6,
+    },
+    {
+      field: "preview",
+      headerName: "Preview",
+      minWidth: 100,
+      flex: 0.8,
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`/product/${params.id}?isEvent=true`}>
+              <Button>
+                <AiOutlineEye size={20} />
+              </Button>
+            </Link>
+          </>
+        );
+      },
+    },
+  ];
+  const row = [];
+  events &&
+    events.forEach((item) => {
+      row.push({
+        id: item._id,
+        name: item.name,
+        price: "US$ " + item.discountPrice,
+        stock: item.stock,
+        sold: item?.soldOut,
+      });
+    });
+  return (
+    <div className="w-full mx-8 pt-1 mt-10 bg-white">
+      <DataGrid
+        rows={row}
+        columns={columns}
+        pageSizeOptions={10}
+        disableRowSelectionOnClick
+      />
+    </div>
+  );
+};
+
+export default AllEvents;
--- frontend/src/components/events/EventCard.jsx
+++ frontend/src/components/events/EventCard.jsx
@@ -15,7 +15,7 @@ const EventCard = ({ active, data }) => {
     if (isItemExists) {
       toast.error("Item already in cart!");
     } else {
-      if (data.stock < 1) {
+      if (data?.stock < 1) {
         toast.error("Product already in cart!");
       } else {
         const cartData = { ...data, qty: 1 };
@@ -31,7 +31,7 @@ const EventCard = ({ active, data }) => {
       } lg:flex p-2`}
     >
       <div className="w-full lg:w-[50%] m-auto">
-        <img src={`${backend_url}${data.images[0]}`} alt="" />
+        <img src={`${backend_url}${data.images[0]?.url}`} alt="" />
       </div>
       <div className="w-full lg:w-[50%] flex flex-col justify-center">
         <h2 className={`${styles.productTitle}`}>{data.name}</h2>
