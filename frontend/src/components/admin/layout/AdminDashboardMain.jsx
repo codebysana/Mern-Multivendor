4cabb10f9fb11a3665dd769a9cc328a41381cb4b
codebysana <sana.younis05@gmail.com>
2025-09-10 20:03:19 +0500

    feat(admin): calculate earning and balance from orders

--- frontend/src/components/admin/layout/AdminDashboardMain.jsx
+++ frontend/src/components/admin/layout/AdminDashboardMain.jsx
@@ -21,6 +21,11 @@ const AdminDashboardMain = () => {
     dispatch(getAllSellers());
   }, [adminOrders]);
 
+  const adminEarning =
+    adminOrders &&
+    adminOrders.reduce((acc, item) => acc + item.totalPrice * 0.1, 0);
+
+  const adminBalance = adminEarning?.toFixed(2);
   const columns = [
     { field: "id", headerName: "Order ID", minWidth: 150, flex: 0.7 },
 
@@ -115,7 +120,9 @@ const AdminDashboardMain = () => {
                   Total Earning{" "}
                 </h3>
               </div>
-              <h5 className="pt-1 pl-[36px] text-[22px] font-[500]">$ 1200</h5>
+              <h5 className="pt-1 pl-[36px] text-[22px] font-[500]">
+                $ {adminBalance}
+              </h5>
             </div>
             {/* 2nd */}
             <div className="w-full mb-4 800px:w-[30%] min-h-[20vh] bg-white shadow rounded  px-2 py-5">
