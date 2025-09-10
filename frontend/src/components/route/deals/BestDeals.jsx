7b6fa647a20f4278c4173c5107e6b14473e10be0
codebysana <sana.younis05@gmail.com>
2025-09-10 20:12:46 +0500

    fix: render product cards from data and allProducts

--- frontend/src/components/route/deals/BestDeals.jsx
+++ frontend/src/components/route/deals/BestDeals.jsx
@@ -20,10 +20,14 @@ const BestDeals = () => {
           <h1>Best Deals</h1>
         </div>
         <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
-          {data &&
-            data.map((item, index) => {
-              <ProductCard data={item} key={index} />;
-            })}
+          {data && data.length !== 0 && (
+            <>
+              {data &&
+                data.map((item, index) => {
+                  <ProductCard data={item} key={index} />;
+                })}
+            </>
+          )}
         </div>
       </div>
     </div>
