32b4133055432242318aa6cfd1a1aab92fbb832a
codebysana <sana.younis05@gmail.com>
2025-09-16 22:06:36 +0500

    chore: add tailwindcss setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,8 @@ import React from "react";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import './input.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
