baa7df1fa48a2f31f364108940dc5362d3616c34
codebysana <sana.younis05@gmail.com>
2025-10-09 11:53:37 +0500

    feat: add secure cookie options (httpOnly, secure, sameSite)

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,9 +5,12 @@ const sendToken = (user, statusCode, res) => {
   // Options for cookies
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
   };
 
-  res.status(statusCode).cookie("token", token.options).json({
+  res.status(statusCode).cookie("token", token, options).json({
     success: true,
     user,
     token,
