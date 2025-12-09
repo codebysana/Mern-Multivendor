f3ade59affc9a623af2352a6f2916e4708e7bbf2
Sana Younas <179322071+codebysana@users.noreply.github.com>
2025-12-10 04:11:20 +0500

    Update server and backend URLs to production

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server = "http://localhost:8000/api/v2";
-export const backend_url = "http://localhost:8000/"
+export const server = "https://multivendor-backend-blond.vercel.app/api/v2";
+export const backend_url = "https://multivendor-backend-blond.vercel.app/";
