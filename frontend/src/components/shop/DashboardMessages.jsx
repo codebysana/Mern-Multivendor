b168ca3c853057a179cb5d1dd2317c8b4e5f7554
Sana Younas <179322071+codebysana@users.noreply.github.com>
2025-12-10 05:43:47 +0500

    Update socket endpoint

--- frontend/src/components/shop/DashboardMessages.jsx
+++ frontend/src/components/shop/DashboardMessages.jsx
@@ -10,7 +10,7 @@ import styles from "../../styles/style";
 import { format } from "timeago.js";
 import { RiGalleryFill } from "react-icons/ri";
 import socketIO from "socket.io-client";
-const ENDPOINT = "http://localhost:4000/";
+const ENDPOINT = "https://multivendor-socket-production.up.railway.app";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const DashboardMessages = () => {
